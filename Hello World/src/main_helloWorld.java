ccc2428d7f44ff73259c4c0240580f7d56adab0a
«Fede-Rosso» <fede.rossolini@gmail.com>
2021-12-02 17:51:11 +0100

    prima modifica

---
+++ Hello World/src/main_helloWorld.java
@@ -0,0 +1,22 @@
+
+public class HelloWorld {  
+/*stiamo definendo la classe HelloWorld
+Un programma Java è costituito da un insieme di classi (almeno una)
+public signifca che questa classe è pubblica: può essere utilizzata da qualunque altra classe del programma
+Il contentuto della classe è racchiuso tra parentesi graffe */
+
+/* void
+defnisce un METODO della class HelloWorld
+Un metodo è una funzionalità (funzione) della classe messa disposizione del resto
+del programma o di altre parti (altri metodi) della stessa classe
+Questo metodo è chiamato main (principale) ed è un metodo speciale. 
+Viene subito eseguito all'inizio del programma.
+*/
+
+public static void main ( String [] args ) {
+
+System . out . println (" Hello World !"); // visualizza un messaggio di saluto
+/* System.out è un oggetto che rappresenta il canale di output standard del sistema (la console...) */
+}
+}
+
